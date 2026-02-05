6e8067521a596c3438a5a8b29546686180a83e11
Yilmar David Murillo <yilmardavidmurillo@gmail.com>
2026-02-05 11:18:56 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 def home(request):
     #return HttpResponse('<h1>Welcome to the page</h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name':'Greg Lim'})
+    return render(request, 'home.html', {'name':'Yilmar Jordan'})
