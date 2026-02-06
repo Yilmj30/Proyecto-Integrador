002300e49e7331dbad1964ca326c51dbf047e05e
Yilmar David Murillo <yilmardavidmurillo@gmail.com>
2026-02-06 16:24:05 -0500

    busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from.models import Movie
 # Create your views here.
 def home(request):
     #return HttpResponse('<h1>Welcome to the page</h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name':'Yilmar Jordan'})
+    #return render(request, 'home.html', {'name':'Yilmar Jordan'})
+    searchTerm = request.GET.get('searchmovie')
+    if searchTerm:
+        movies = Movie.objects.filter(tittle__icontains=searchTerm)
+    else: 
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies': movies})
+
+
+def about(request):
+    return HttpResponse('<h1>Welcome to About Page</h1>')
